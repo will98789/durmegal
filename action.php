11faf949e5a97787f8d93ea5d391d4dadb431538
Roshni Vachhani <Roshni.Vachhani@gmail.com>
2020-11-14 15:20:52 +0000

    Update action.php

--- action.php
+++ action.php
@@ -1,1 +1,8 @@
-
+<?php
+echo ("done :)");
+if( isset($_POST['name'] ) && isset( $_POST['email'] ) )
+{
+    $txt= $_POST['name'].','.$_POST['email'].','.$_POST['football'].','.$_POST['badminton'].','.$_POST['rowing'].','.$_POST['netball'].','.$_POST['comedy'].','.$_POST['romcom'].','.$_POST['thriller'].','.$_POST['reading'].','.$_POST['painting'].','.$_POST['writing'] . PHP_EOL; 
+    file_put_contents('data.txt', $txt, FILE_APPEND);
+}
+?>
