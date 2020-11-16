4ec6ef7f6985bc59bfa81edac9d4de311d82fa8a
Will <will947@hotmail.co.uk>
2020-11-16 18:43:50 +0000

    Added scrolling styling

--- connect.js
+++ connect.js
@@ -1,4 +1,4 @@
-// client-side
+//Set settings for connection
 const socket = io("http://167.172.60.28:8080", {
   withCredentials: false,
   extraHeaders: {
@@ -7,24 +7,37 @@ const socket = io("http://167.172.60.28:8080", {
   transports: ['websocket']
 });
 
-// submit text message without reload/refresh the page
+//Submit text message without reload/refresh the page
 $('form').submit(function(e){
     e.preventDefault(); // prevents page reloading
+    //Emit chat message
     socket.emit('chat_message', $('#txt').val());
     $('#txt').val('');
     return false;
 });
 
-// append the chat text message
+//Append the chat text message
 socket.on('chat_message', function(msg){
     $('#messages').append($('<li>').html(msg));
+    updateScroll();
 });
 
-// append text if someone is online
+//Append text if someone is online
 socket.on('is_online', function(username) {
     $('#messages').append($('<li>').html(username));
 });
 
-// ask username
+//Open text box
+socket.on('found_partner',function(){
+    document.getElementById('txt').readOnly = false;
+})
+
+//Get username
 var username = prompt('Please tell me your name');
 socket.emit('username', username);
+
+//Keep text box scrolled to bottom
+function updateScroll(){
+  var element = document.getElementById("messagesBox");
+  element.scrollTop = element.scrollHeight;
+}
